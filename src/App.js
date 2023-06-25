6f6ff7686366f549fc6bd88fc6673f67adb09e60
vernetto <Ernesto0z>
2023-06-25 21:50:40 +0200

    adding logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [sentences, setSentences] = useState([]);
+  const [currentSentence, setCurrentSentence] = useState(null);
+  const [userInput, setUserInput] = useState("");
+  const [score, setScore] = useState(0);
+
+  // Load sentences from JSON file
+  useEffect(() => {
+    fetch("/sentences.json")
+      .then(res => res.json())
+      .then(data => {
+        setSentences(data);
+        setCurrentSentence(data[0]);
+      });
+  }, []);
+
+  const handleInputChange = event => {
+    setUserInput(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (userInput === currentSentence.answer) {
+      setScore(score + 1);
+    }
+    // Go to next sentence
+    const currentIndex = sentences.indexOf(currentSentence);
+    const nextSentence = sentences[currentIndex + 1];
+    setCurrentSentence(nextSentence);
+    setUserInput(""); // Reset user input
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {currentSentence && <h1>{currentSentence.sentence}</h1>}
+      <input type="text" value={userInput} onChange={handleInputChange} />
+      <button onClick={handleSubmit}>Submit</button>
+      <h2>Score: {score}</h2>
     </div>
   );
 }
