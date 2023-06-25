9e84a1aba24a95a9762ff5dcd1d09eb3405df4c8
vernetto <Ernesto0z>
2023-06-25 22:35:27 +0200

    adding logic

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [sentences, setSentences] = useState([]);
-  const [currentSentence, setCurrentSentence] = useState(null);
+  const [currentSentenceIndex, setCurrentSentenceIndex] = useState(0);
   const [userInput, setUserInput] = useState("");
   const [score, setScore] = useState(0);
+  const [answeredCorrectly, setAnsweredCorrectly] = useState({});
 
   // Load sentences from JSON file
   useEffect(() => {
@@ -12,7 +13,6 @@ function App() {
       .then(res => res.json())
       .then(data => {
         setSentences(data);
-        setCurrentSentence(data[0]);
       });
   }, []);
 
@@ -21,22 +21,47 @@ function App() {
   };
 
   const handleSubmit = () => {
-    if (userInput === currentSentence.answer) {
+      // Check if this sentence has been answered correctly before
+      if (answeredCorrectly[currentSentenceIndex]) {
+        alert('You have already correctly answered this sentence.');
+        return;
+      }
+    if (userInput === sentences[currentSentenceIndex].answer) {
       setScore(score + 1);
+    setAnsweredCorrectly({
+      ...answeredCorrectly,
+      [currentSentenceIndex]: true, // Mark this sentence as answered correctly
+    });
     }
-    // Go to next sentence
-    const currentIndex = sentences.indexOf(currentSentence);
-    const nextSentence = sentences[currentIndex + 1];
-    setCurrentSentence(nextSentence);
+    handleNext();
+    setUserInput(""); // Reset user input
+  };
+
+  const handleNext = () => {
+    // Prevents going beyond the last sentence
+    if (currentSentenceIndex < sentences.length - 1) {
+      setCurrentSentenceIndex(currentSentenceIndex + 1);
+    }
+    setUserInput(""); // Reset user input
+  };
+
+  const handlePrevious = () => {
+    // Prevents going before the first sentence
+    if (currentSentenceIndex > 0) {
+      setCurrentSentenceIndex(currentSentenceIndex - 1);
+    }
     setUserInput(""); // Reset user input
   };
 
   return (
     <div className="App">
-      {currentSentence && <h1>{currentSentence.sentence}</h1>}
+      {sentences.length > 0 && <h1>{sentences[currentSentenceIndex].sentence}</h1>}
       <input type="text" value={userInput} onChange={handleInputChange} />
       <button onClick={handleSubmit}>Submit</button>
+      <button onClick={handlePrevious} disabled={currentSentenceIndex === 0}>Previous</button>
+      <button onClick={handleNext} disabled={currentSentenceIndex === sentences.length - 1}>Next</button>
       <h2>Score: {score}</h2>
+      <h3>Sentence: {currentSentenceIndex + 1} of {sentences.length}</h3>
     </div>
   );
 }
